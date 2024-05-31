b045505b8586f3bede69bbd742a5ed77c048d7c6
Sile9t <eyesless1jack@gmail.com>
2024-05-31 14:48:46 +0300

    Added service class and his initialization

---
+++ src/ru/gb/educ_stream/Service.java
@@ -0,0 +1,10 @@
+package ru.gb.educ_stream;
+
+public class Service {
+    private Group group;
+    private StudentBuilder builder;
+    public Service() {
+        group = new Group();
+        builder = new StudentBuilder();
+    }
+}
