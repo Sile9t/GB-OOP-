1bc40d4014f75b8dd486816d668ec931ec338973
Sile9t <eyesless1jack@gmail.com>
2024-05-31 14:59:37 +0300

    Added main class

--- src/ru/gb/educ_stream/StudentBuilder.java
+++ src/ru/gb/educ_stream/StudentBuilder.java
@@ -3,6 +3,6 @@ package ru.gb.educ_stream;
 public class StudentBuilder {
     private long genId;
     public Student build(String name, int age){
-        return new Student(genId++, name, age); 
+        return new Student(genId++, name, age);
     }
 }
