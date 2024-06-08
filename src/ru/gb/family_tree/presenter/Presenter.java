17086d793d397578541bbfc78e4af68e7a3c3562
Sile9t <eyesless1jack@gmail.com>
2024-06-08 18:19:05 +0300

    Added Presentre ctor

--- src/ru/gb/family_tree/presenter/Presenter.java
+++ src/ru/gb/family_tree/presenter/Presenter.java
@@ -1,5 +1,14 @@
 package presenter;
 
+import model.services.Service;
+import view.View;
+
 public class Presenter {
-    
+    private View view;
+    private Service service;
+
+    public Presenter(View view){
+        this.view = view;
+        this.service = new Service();
+    }
 }
