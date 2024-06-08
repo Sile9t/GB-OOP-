bf9ffbb38a2fb8caa24a36fef97cd6b1aa443b5b
Sile9t <eyesless1jack@gmail.com>
2024-06-08 18:50:34 +0300

    Added ConsoleView ctor

--- src/ru/gb/educ_stream/view/ConsoleView.java
+++ src/ru/gb/educ_stream/view/ConsoleView.java
@@ -1,5 +1,21 @@
 package ru.gb.educ_stream.view;
 
+import java.util.Scanner;
+
 public class ConsoleView implements View {
+    private Scanner scanner;
+    
+    public ConsoleView() {
+        scanner = new Scanner(System.in);
+    }
+    @Override
+    public void start() {
+            
+    }
+
+    @Override
+    public void printAnswer(String answer) {
+        System.out.println(answer);
+    }
     
 }
--- src/ru/gb/educ_stream/view/View.java
+++ src/ru/gb/educ_stream/view/View.java
@@ -2,5 +2,5 @@ package ru.gb.educ_stream.view;
 
 public interface View {
     void start();
-    void printAnswer();
+    void printAnswer(String answer);
 }
