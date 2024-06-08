58c1835890ad840f682949ed7ec2e1de17b8bb3c
Sile9t <eyesless1jack@gmail.com>
2024-06-08 18:44:39 +0300

    Added Presenter ctor

--- src/ru/gb/educ_stream/presenter/Presenter.java
+++ src/ru/gb/educ_stream/presenter/Presenter.java
@@ -1,5 +1,13 @@
 package ru.gb.educ_stream.presenter;
 
+import ru.gb.educ_stream.model.services.Service;
+import ru.gb.educ_stream.view.View;
+
 public class Presenter {
-    
+    private View view;
+    private Service service;
+    public Presenter(View view) {
+        this.view = view;
+        service = new Service();
+    }
 }
