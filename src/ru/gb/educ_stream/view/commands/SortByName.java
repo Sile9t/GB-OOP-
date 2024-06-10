dbe7de46e4c005cfc8761da0ec7b7d839acec900
Sile9t <eyesless1jack@gmail.com>
2024-06-10 14:18:43 +0300

    Added SortByName command

---
+++ src/ru/gb/educ_stream/view/commands/SortByName.java
@@ -0,0 +1,13 @@
+package ru.gb.educ_stream.view.commands;
+
+import ru.gb.educ_stream.view.ConsoleView;
+
+public class SortByName extends Command {
+
+    public SortByName(ConsoleView view) {
+        super(view, "Sort by name");
+    }
+    void execute(){
+        getConsoleView().getStudentsListInfo();
+    }    
+}
