c2d2c3019bf1972522812104f9e130e6087d7589
Sile9t <eyesless1jack@gmail.com>
2024-06-10 14:09:16 +0300

    Added Command implemented class

---
+++ src/ru/gb/educ_stream/view/commands/Command.java
@@ -0,0 +1,16 @@
+package ru.gb.educ_stream.view.commands;
+
+import ru.gb.educ_stream.view.ConsoleView;
+
+public abstract class Command {
+    private ConsoleView view;
+    private String description;
+    public Command(ConsoleView view, String description) {
+        this.view = view;
+        this.description = description;
+    }
+    public String getDescription(){
+        return description;
+    }
+    abstract void execute();
+}
