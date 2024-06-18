703a0210478484e0e98bea13a7a33e0ad7fe6632
Sile9t <eyesless1jack@gmail.com>
2024-06-18 11:25:54 +0300

    Added SmallBusinessCardBuild

--- src/ru/gb/creational/builder/SmallBusinessCardBuild.java
+++ src/ru/gb/creational/builder/SmallBusinessCardBuild.java
@@ -1,5 +1,20 @@
 package ru.gb.creational.builder;
 
-public class SmallBusinessCardBuild {
+public class SmallBusinessCardBuild extends BusinessCardBuilder {
+
+    @Override
+    public void createName() {
+        getCard().setName("Small buisness card");
+    }
+
+    @Override
+    public void createPrise() {
+        getCard().setPrice(50);
+    }
+
+    @Override
+    public void createSize() {
+        getCard().setSize(new int[] {5, 17, 29});
+    }
     
 }
