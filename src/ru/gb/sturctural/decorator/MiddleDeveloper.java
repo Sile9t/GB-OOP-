9145c6f85503c79748b755340f3e78a9254bbf05
Sile9t <eyesless1jack@gmail.com>
2024-06-18 12:45:54 +0300

    Implemented DeveloperDecorator in MiddleDeveloper class

--- src/ru/gb/sturctural/decorator/MiddleDeveloper.java
+++ src/ru/gb/sturctural/decorator/MiddleDeveloper.java
@@ -1,11 +1,17 @@
 package ru.gb.sturctural.decorator;
 
-public class MiddleDeveloper implements Developer {
+public class MiddleDeveloper extends DeveloperDecorator {
+
+    public MiddleDeveloper(Developer developer) {
+        super(developer);
+    }
 
     @Override
     public String makeJob() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeJob'");
+        return super.makeJob() + "\n" + codeReview();
     }
     
+    private String codeReview(){
+        return "Doing code review";
+    }
 }
