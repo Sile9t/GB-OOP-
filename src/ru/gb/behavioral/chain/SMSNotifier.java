86babe32b519eb27d7163f65511a727dc58c33bb
Sile9t <eyesless1jack@gmail.com>
2024-06-18 13:58:16 +0300

    Added SMSNotifier class extends Notifier

---
+++ src/ru/gb/behavioral/chain/SMSNotifier.java
@@ -0,0 +1,14 @@
+package ru.gb.behavioral.chain;
+
+public class SMSNotifier extends Notifier {
+
+    public SMSNotifier(int priority) {
+        super(priority);
+\    }
+
+    @Override
+    void write(String message) {
+        System.out.println("Sending sms notification: " + message);
+    }
+    
+}
