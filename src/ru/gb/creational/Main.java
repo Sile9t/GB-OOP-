3f89930ed49aa5fb4550b47ca470ade123ef5320
Sile9t <eyesless1jack@gmail.com>
2024-06-18 11:37:48 +0300

    Implemented Main class

--- src/ru/gb/creational/Main.java
+++ src/ru/gb/creational/Main.java
@@ -1,9 +1,15 @@
 package ru.gb.creational;
 
-import ru.gb.creational.singleton.SimpleResource;
+import ru.gb.creational.builder.BusinessCard;
+import ru.gb.creational.builder.BusinessCardBuilder;
+import ru.gb.creational.builder.LargeBusinessCardBuild;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleResource simpleResource = SimpleResource.getSimpleResource();
+        BusinessCardBuilder builder = new LargeBusinessCardBuild();
+
+        BusinessCard card = builder.build();
+        
+        System.out.println(card);
     }
 }
