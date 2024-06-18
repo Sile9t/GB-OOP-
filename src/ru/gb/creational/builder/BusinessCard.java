d0022c21ac7826ccdda7f3b7ac5c06e90b6fe88e
Sile9t <eyesless1jack@gmail.com>
2024-06-18 11:14:57 +0300

    Implemented BusinessCard class

--- src/ru/gb/creational/builder/BusinessCard.java
+++ src/ru/gb/creational/builder/BusinessCard.java
@@ -1,5 +1,23 @@
 package ru.gb.creational.builder;
 
+import java.util.Arrays;
+
 public class BusinessCard {
+    private String name;
+    private double price;
+    private int[] size;
     
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setSize(int[] size) {
+        this.size = size;
+    }
+    @Override
+    public String toString() {
+        return "BusinessCard [name=" + name + ", price=" + price + ", size=" + Arrays.toString(size) + "]";
+    }
 }
