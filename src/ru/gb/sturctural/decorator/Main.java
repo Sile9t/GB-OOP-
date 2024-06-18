4963e108d7c12721697061caf66095e13d78d26f
Sile9t <eyesless1jack@gmail.com>
2024-06-18 12:45:54 +0300

    Implemented DeveloperDecorator in MiddleDeveloper class

--- src/ru/gb/sturctural/decorator/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.sturctural.decorator;
-
-public class Main {
-    public static void main(String[] args) {
-        JuniorDeveloper jun = new JuniorDeveloper();
-
-        System.out.println(jun.makeJob());
-    }
-}
