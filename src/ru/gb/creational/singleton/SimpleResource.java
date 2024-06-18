aa70162a6605dc9507190f836c56599d6251d6c8
Sile9t <eyesless1jack@gmail.com>
2024-06-18 11:02:55 +0300

    Added SimpleResource.getSimpleResource method

--- src/ru/gb/creational/singleton/SimpleResource.java
+++ src/ru/gb/creational/singleton/SimpleResource.java
@@ -1,5 +1,12 @@
 package ru.gb.creational.singleton;
 
 public class SimpleResource {
+    private static SimpleResource simpleResource;
     
+    public static SimpleResource getSimpleResource(){
+        if (simpleResource == null)
+            simpleResource = new SimpleResource();
+        
+        return simpleResource;
+    }
 }
